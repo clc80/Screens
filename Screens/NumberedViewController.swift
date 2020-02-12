635005856127650824026c887109bb60a40b378d
Claudia Maciel <clc_80@yahoo.com>
2020-02-11 19:17:39 -0800

    added a subclass

---
+++ Screens/NumberedViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NumberedViewController.swift
+//  Screens
+//
+//  Created by Claudia Contreras on 2/11/20.
+//  Copyright © 2020 thecoderpilot. All rights reserved.
+//
+
+import UIKit
+
+class NumberedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
